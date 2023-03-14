d395e37f7db05613213fafdfefa33c794c200c87
gustavomorais <gustavomorais.dev@gmail.com>
2023-03-14 16:47:20 -0300

    completa o requisito 1 que mapeia as rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import Search from './components/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
-    // Iniciando
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
+            <Route exact path="/" component={ Login } />
+            <Route path="/*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
